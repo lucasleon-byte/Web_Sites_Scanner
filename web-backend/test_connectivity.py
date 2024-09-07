bb51823af664da9e1c5e9042b36c3db3c95c41e1
Lucas Vunic <v.lucasleon@gmail.com>
2024-09-07 07:38:09 +0200

    editing files to match criteria

--- web-backend/test_connectivity.py
+++ web-backend/test_connectivity.py
@@ -3,18 +3,18 @@ from pymongo.errors import PyMongoError
 
 def test_mongo_connection():
     try:
-        # Zamijenite ovo sa svojom konekcijskom strunom
+        
         client = MongoClient("mongodb+srv://leonvunic:Nogomet123@clusterdiplomski.rqewm.mongodb.net/?retryWrites=true&w=majority&ssl=true")
         
-        # Pokušajte dohvatiti informacije o serveru
+        
         server_info = client.server_info() 
-        print("Uspješna konekcija s MongoDB!")
+        print("Uspješna konekcija")
         print("Server info:", server_info)
         
     except ConnectionError:
-        print("Neuspjela konekcija s MongoDB. Provjerite konekcijski string.")
+        print("Neuspjela konekcija ")
     except Exception as e:
-        print(f"Dogodila se greška: {e}")
+        print(f"greška: {e}")
 
 if __name__ == "__main__":
     test_mongo_connection()
